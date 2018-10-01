0e439d1fe144aebc8da2cb2bf530a71c50ebb13a
DawnOfCremation <damians@gmail.com>
2018-10-01 21:41:18 +0930

    Blog Functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,16 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
-    body = models.CharField(max_length=200)
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        #changes the blog title in the admin area from being called blog 1 and blog 1 to the actual blog title
+        return self.title
+
+    def summary(self):
+        #cap the text to 100
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        #changes the blog title in the admin area from being called blog 1 and blog 1 to the actual blog title
+        return self.summary
