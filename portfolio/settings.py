7a2016bdc680732e50a7177ee45b1b34ff988627
DawnOfCremation <damians@gmail.com>
2018-10-06 03:12:24 +0930

    settings db update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'bundy',
-        'PASSWORD':'evildead666',
+        'USER': 'dawnofcremation',
+        'PASSWORD':'odinjem',
         'HOST': 'localhost',
         'PORT': '',
     }
