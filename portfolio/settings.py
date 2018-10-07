c7b901025dbe9bd60f6d9d8fa7ccb019ba6b3667
DawnOfCremation <damians@gmail.com>
2018-10-07 22:19:41 +1030

    model image and settings logging

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -86,6 +86,41 @@ DATABASES = {
     }
 }
 
+"""
+#Local --Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'evildead666',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+"""
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
+
+
 #delete
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
